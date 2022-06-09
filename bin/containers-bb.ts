e04f1e414b2f89965935ba361a21a189382e1f41
EC2 Default User <ec2-user@ip-172-31-45-2.us-east-2.compute.internal>
2022-06-09 16:19:07 +0000

    Setting up EKS Blueprints deployment pipeline

---
+++ bin/containers-bb.ts
@@ -0,0 +1,13 @@
+// bin/containers-bb.ts
+import * as cdk from 'aws-cdk-lib';
+import ClusterConstruct from '../lib/containers-bb-stack';
+import PipelineConstruct from '../lib/pipeline';
+
+
+const app = new cdk.App();
+const account = process.env.CDK_DEFAULT_ACCOUNT!;
+const region = process.env.CDK_DEFAULT_REGION;
+const env = { account, region }
+
+new ClusterConstruct(app, 'cluster', { env });
+new PipelineConstruct(app, 'pipeline', { env });
